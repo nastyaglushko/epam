559afd220383789b146e4372bd46adef81be0c61
nastya <nastya.glushko14@gmail.com>
2020-03-18 19:13:05 +0200

    some fix

--- lab3/javascripts/task1.js
+++ lab3/javascripts/task1.js
@@ -5,36 +5,36 @@ const menNames = ["Sheldon", "Jake", "James", "Tony"];
 const cities = ["Lviv", "London", "Amsterdam", "New York"];
 const jobs = ["Dancer", "Developer", "Engineer", "Manager"];
 
-var userName = "Username";
+let userName = "Username";
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 function getMarried(sex) {
-  let name;
-  if (sex === "w") name = womenNames[getRandomInt(womenNames.length)];
-  else name = menNames[getRandomInt(menNames.length)];
-  return `${userName}, you'll get married with ${name} and you'll have ${getRandomInt(
+    let name;
+    if (sex === "w") name = womenNames[getRandomInt(womenNames.length)];
+    else name = menNames[getRandomInt(menNames.length)];
+    return `${userName}, you'll get married with ${name} and you'll have ${getRandomInt(
     10
   )} children`;
 }
 
 function getWork() {
-  return `${userName}, you'll move to ${
+    return `${userName}, you'll move to ${
     cities[getRandomInt(cities.length)]
   } to work as ${jobs[getRandomInt(jobs.length)]}`;
 }
 
 playButton.onclick = function() {
-  var userInput = prompt("What is your name?", userName);
-  if (userInput === null) alert("You cancelled game.");
-  else {
-    if (userInput !== "") userName = userInput;
-    if (confirm("Do you prefer women?")) {
-      alert(getMarried("w"));
-    } else {
-      alert(getMarried("m"));
+    let userInput = prompt("What is your name?", userName);
+    if (userInput === null) alert("You cancelled game.");
+    else {
+        if (userInput !== "") userName = userInput;
+        if (confirm("Do you prefer women?")) {
+            alert(getMarried("w"));
+        } else {
+            alert(getMarried("m"));
+        }
     }
-  }
 };
